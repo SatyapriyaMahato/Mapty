1885ff41dc2adcabedc736f20d6c69d1d4d6d02f
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-09-06 18:30:46 +0530

    adding leaflet.js cdn

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(function (position) {
+
+
+
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    var marker = L.marker([51.5, -0.09]).addTo(map);
+
+    console.log(position);
+
+
+}, function () {
+    console.log("couldn't get your position");
+
+
+
+});
