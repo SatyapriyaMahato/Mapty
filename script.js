e7cc2da57587902776a88ac6dad50ef4e5635238
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-09-07 13:21:05 +0530

    add input validation

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ class Cycling extends Workout {
     }
 
     calcSpeed() {
-        this.speed = this.distance / this.duration;
+        this.speed = this.distance / (this.duration / 60);
         return this.speed;
     }
 }
@@ -110,6 +110,38 @@ class App {
 
     _newWorkout(e) {
         e.preventDefault();
+
+        const validInputs = (...inputs) => inputs.every(inp => Number.isFinite(inp));
+        const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+        //  Get data from the form
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+
+
+        // check if the data is valid or not
+        if (type === "running") {
+            const cadence = +inputCadence.value;
+            if (!validInputs(distance, duration, cadence) || !allPositive(distance, duration, cadence)) {
+                return alert("Enter valid input");
+
+            }
+        }
+        if (type === "cycling") {
+            const elevation = +inputElevation.value;
+            if (!validInputs(distance, duration, elevation) || !allPositive(distance, duration)) {
+                return alert("Enter valid input");
+
+            }
+        }
+
+        // if workout is running create running object
+
+        // if workout is cycling create cycling onjetcy
+
+        //  add new objetc to workout array
+
+        // render workout on map as a marker
 
         inputDistance.value = inputDuration.value = inputElevation.value = inputCadence.value = '';
 
